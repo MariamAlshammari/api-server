c65c1ac09ebdaca72b4f31d7370c33d0cf2d1f2f
mariamalshammari <malshammari37@gmail.com>
2021-08-19 15:46:42 +0300

    lab04

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@
 const express=require('express');
 const app=express();
 
-const validatorMiddleware=require('./middleware/validator'); 
+// const validatorMiddleware=require('./middleware/validator'); 
 
 const loggerMiddleware=require('./middleware/logger');
 
